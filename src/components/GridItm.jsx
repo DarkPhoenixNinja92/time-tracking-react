8379992a3bcc22ad93b98bd6e4272b10e504b075
Shaun <scpour@gmail.com>
2024-01-02 23:33:49 -0500

    Dividing into components

---
+++ src/components/GridItm.jsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+export default class GridItm extends Component {
+  render() {
+    return (
+        <div className="grid-itm">
+            
+        </div>
+    )
+  }
+}
